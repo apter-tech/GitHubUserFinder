4f4ce3c1a92c5f3e3084645d57bf79b74ee4c6cd
Imre Kaszab <imre.kaszab@supercharge.io>
2022-04-15 18:26:58 +0200

    feat(list): add empty state

---
+++ androidApp/src/main/java/io/imrekaszab/githubuserfinder/android/ui/widget/EmptyView.kt
@@ -0,0 +1,21 @@
+package io.imrekaszab.githubuserfinder.android.ui.widget
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun EmptyView() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "We don't have any content, sorry \uD83D\uDE14")
+        }
+    }
+}
