1438283ac44e8b9b54c528fd35cfe9a03150cc25
Imre Kaszab <imre.kaszab@supercharge.io>
2022-05-27 08:08:10 +0200

    feat(paging): add paging functionality to list screen

---
+++ iosApp/iosApp/UI/Widget/ClearButtonModifier.swift
@@ -0,0 +1,25 @@
+//
+//  ClearButtonModifier.swift
+//  iosApp
+//
+//  Created by Imre Kaszab on 2022. 04. 22..
+//  Copyright © 2022. orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct ClearButtonModifier: ViewModifier {
+    @Binding var text: String
+
+    public func body(content: Content) -> some View {
+        ZStack(alignment: .trailing) {
+            content
+            if !text.isEmpty {
+                Button(action: { self.text = "" }) {
+                    Image(systemName: "delete.left")
+                        .foregroundColor(Color(UIColor.opaqueSeparator))
+                }
+            }
+        }
+    }
+}
